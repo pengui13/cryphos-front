1d26b0b62f6c179bd25ebdf9863dcb932f9aca1d
Sklyar13OFF <dansklyarov13@gmail.com>
2025-11-28 18:57:55 +0100

    wtf

--- app/api/ApiWrapper.jsx
+++ app/api/ApiWrapper.jsx
@@ -13,9 +13,9 @@ const IntervalsMapping = {
 };
 const cookies = new Cookies();
 
-export const BASE_URL = "http://127.0.0.1:8000/api/";
-export const MEDIA_URL = "http://127.0.0.1:8000";
-export const WEBSOCKET_URL = "ws://127.0.0.1:8000/ws/";
+export const BASE_URL = "https://cryphos.com/api/";
+export const MEDIA_URL = "https://cryphos.com";
+export const WEBSOCKET_URL = "ws://cryphos.com/ws/";
 export const BASE_FRONT = "http://localhost:3000/";
 
 // Helpers
