ae4ae67f982c5a12da4c67698782b8c688d40c4f
Sklyar13OFF <dansklyarov13@gmail.com>
2026-02-13 19:52:16 +0100

    downgrade

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,6 @@
-// postcss.config.js
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
-    'postcss-preset-env': {
-      stage: 3,
-      features: {
-        'nesting-rules': true,
-        'custom-properties': true,
-      },
-      autoprefixer: {
-        grid: 'autoplace',
-      },
-    },
-    'autoprefixer': {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 }
